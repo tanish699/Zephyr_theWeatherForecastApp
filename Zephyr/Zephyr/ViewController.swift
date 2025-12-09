915390f6e7683cdefd56d81115cd98357899f5dd
Tanish Wadhwa <tanish2149.be23@chitkara.edu.in>
2025-12-09 22:36:29 +0530

    Created Ui Part Through Story board

---
+++ Zephyr/Zephyr/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Zephyr
+//
+//  Created by Tanish on 09/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var gradientView: UIView!
+    
+    
+    @IBOutlet weak var cardView: UIView!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    @IBOutlet weak var weatherIconView: UIImageView!
+    
+    @IBOutlet weak var forecastStackView: UIStackView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
